79b4a67a0c9f542f176a18e03f200ffd920446b4
Robert Schulte <schulte.robertw@outlook.com>
2018-10-22 21:05:59 -0500

    checked for roaches

--- bin/run.rb
+++ bin/run.rb
@@ -1,9 +1,29 @@
 require_relative '../config/environment'
 
-def yelp_api_request(url)
-  # binding.pry
-  JSON.parse(RestClient.get("#{url}", :Authorization => "Bearer ZCGB7cghN6Tbki6ojzII7FrWTpKUxQ3FRxvxjOG1YSBFZg7LS8TULAO6gQtFodUK1ku8xcImJBqvAlqS1uAN_zhmXxyH2TYb7qHhSrB77GASx8wH_WRJOrDMCzDOW3Yx"))
-end
+# https://www.yelp.com/developers/documentation/v3/authentication
+# https://www.yelp.com/developers/documentation/v3/business_reviews
+# https://www.rubydoc.info/gems/rest-client/RestClient/Request
+#
+# def yelp_api_request(url)
+#   JSON.parse(RestClient.get("#{url}", :Authorization => "Bearer ZCGB7cghN6Tbki6ojzII7FrWTpKUxQ3FRxvxjOG1YSBFZg7LS8TULAO6gQtFodUK1ku8xcImJBqvAlqS1uAN_zhmXxyH2TYb7qHhSrB77GASx8wH_WRJOrDMCzDOW3Yx"))
+# end
 
-
-puts "HELLO WORLD"
+# puts "query the data in some way"
+# user_input = gets.chomp
+#
+# while user_input != 'exit'
+#   case user_input
+#     when "xyz"
+#       call_xyz
+#     when "123"
+#       call_123
+#     when "abc"
+#       call_abc
+#     else "anything else"
+#       call_error
+#     end
+#   puts "query the data in some way"
+#   user_input = gets.chomp
+# end
+#
+# puts "thanks for visiting"
