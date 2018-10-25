c5ca799f8cc12c4f6629a10cc0afb958772991f5
Robert Schulte <schulte.robertw@outlook.com>
2018-10-25 16:49:59 -0500

    wrote more cli script

--- cli_interface.rb
+++ cli_interface.rb
@@ -1,23 +1,26 @@
 require_relative 'config/environment'
 
-$bar_types = {
-  "sports" => 0,
-  "drag" => 1,
-  "brewery" => 2,
-  "cocktail" => 3,
-  "irish pub" => 4,
-  "gastropub" => 5,
-  "lounge" => 6,
-  "club" => 7,
-  "dive" => 8,
-  "wine" => 9,
+$menu_choices = {
+  "Search Houston bars by name" => 0,
+  "Search Houston's nastiest bars" => 1,
+  "Ask an expert" => 2
 }
 
+$nasty_menu_choices = {
+  "Use our nasty words" => 0,
+  "Use your own nasty word" => 1
+}
+
+$expert_menu_choices = {
+  "Find which bars are hated by Houston's angriest patron" => 0,
+  "Worst date spot in town" => 1,
+  "Where to take your sidepiece - ask an expert" => 2
+}
 
 $prompt = TTY::Prompt.new
 
 def welcome_get_name
-  name = $prompt.ask("Welcome to the Houston Shady Bar Search. What's your name?", default: "Anonymous") do |q|
+  name = $prompt.ask("Welcome to the Houston Shady Bar Search. What's your name?") do |q|
     q.required true
     q.validate /\A\w+\Z/
     q.modify   :capitalize
@@ -25,44 +28,63 @@ def welcome_get_name
 end
 
 def move_on(name)
-  move_on = $prompt.yes?("#{name}, are you ready to explore the seedy underbelly of Houston?") do |q|
-    q.required true
-  end
+  move_on = $prompt.select("\n#{name}, are you ready to explore the seedy underbelly of Houston?", %w(Yes No))
 end
 
 def exit
   puts "Go back to your sheltered, uninteresting life!"
 end
 
-def continue
-  move_on_again = $prompt.yes?("Are you sure you can handle it?")
-  if move_on_again == false
-    puts "You're probably right. Let's do this anyway."
+def continue_message
+  move_on_again = $prompt.select("\nAre you sure you can handle it?", %w(I\ was\ born\ ready Probably\ not))
+  if move_on_again == "Probably not"
+    print "\nYou're probably right. Let's do this anyway. "
+    return "scaredy cat"
   else
-    puts "Oh, you bad!"
+    print "\nOh, you bad! "
+    return "bad boy"
   end
 end
 
-def start_menu
-  categories = $prompt.multi_select("Select the bar types you'd like to explore. Hit space bar to make a selection, and hit enter when you are finished adding selections.", $bar_types)
+def continue(name)
+  choice = start_menu(name)
+
+  case choice
+  when 0
+
+  when 1
+
+  when 2
+
+  end
 end
 
-# def execute
-#
-# def run_program
-#   name = welcome_get_name
-#   yes_or_no = move_on(name)
-#
-#   if yes_or_no == false
-#     exit
-#   else
-#     continue
-#   end
-#
-#   choices = start_menu
-#   case choices
-#   when <condition>
-#   when <condition>
-#   when <condition>
-#   end
-# end
+def start_menu(name)
+  categories = $prompt.select("Alright #{name}, pick your poison:", $menu_choices)
+end
+
+def side_piece
+  result = User.side_pieces
+
+
+  # { name: name, recommendation: bar, review: review.content }
+end
+
+def validate_input(input)
+
+end
+
+def run_program
+  name = welcome_get_name
+  yes_or_no = move_on(name)
+
+  if yes_or_no == "No"
+    exit
+  else
+    name = continue_message
+    continue(name)
+  end
+
+end
+
+run_program
